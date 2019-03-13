5faa2c8eb7cbdfa865c7044135934ec7acd4f30c
Adam Sawicki <sawickiap@poczta.onet.pl>
2019-03-13 21:11:43 +0100

    Initialized Direct3D 11 and rendering some frames - clearing background for now.

--- D3d11Sample.cpp
+++ D3d11Sample.cpp
@@ -25,6 +25,7 @@
 
 const wchar_t* const WINDOW_CLASS_NAME = L"WIN_FONT_RENDER_SAMPLE_D3D11";
 const wchar_t* const WINDOW_TITLE = L"WinFontRender Direct3D 11 Sample";
+const uvec2 DISPLAY_SIZE = uvec2(1280, 720);
 
 HINSTANCE g_Instance;
 
@@ -46,18 +47,19 @@ public:
     void Frame();
 
 private:
-    CComPtr<IDXGIFactory> m_DxgiFactory;
     HWND m_Wnd = NULL;
+    CComPtr<IDXGIFactory> m_DxgiFactory;
+    CComPtr<ID3D11Device> m_Dev;
+    CComPtr<ID3D11DeviceContext> m_Ctx;
+    CComPtr<IDXGISwapChain> m_SwapChain;
+    CComPtr<ID3D11Texture2D> m_SwapChainTexture;
+    CComPtr<ID3D11RenderTargetView> m_SwapChainRTV;
 };
 
 static std::unique_ptr<CApp> g_App;
 
 CApp::CApp()
 {
-    HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&m_DxgiFactory);
-    assert(SUCCEEDED(hr));
-
-    // TODO init device
 }
 
 CApp::~CApp()
@@ -67,16 +69,56 @@ CApp::~CApp()
 void CApp::Init(HWND wnd)
 {
     m_Wnd = wnd;
+
+    HRESULT hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&m_DxgiFactory);
+    assert(SUCCEEDED(hr));
+
+    hr = D3D11CreateDevice(
+        NULL,
+        D3D_DRIVER_TYPE_HARDWARE,
+        nullptr, // Software
+        D3D11_CREATE_DEVICE_SINGLETHREADED,
+        NULL,
+        0, // FeatureLevels
+        D3D11_SDK_VERSION,
+        &m_Dev,
+        nullptr, // pFeatureLevel
+        &m_Ctx);
+    assert(SUCCEEDED(hr));
+
+    DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+    swapChainDesc.BufferDesc.Width  = DISPLAY_SIZE.x;
+    swapChainDesc.BufferDesc.Height = DISPLAY_SIZE.y;
+    swapChainDesc.BufferDesc.RefreshRate = {0, 0};
+    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+    swapChainDesc.SampleDesc.Count = 1;
+    swapChainDesc.SampleDesc.Quality = 0;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.BufferCount = 3;
+    swapChainDesc.OutputWindow = wnd;
+    swapChainDesc.Windowed = TRUE;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+    swapChainDesc.Flags = 0;
+    swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+    hr = m_DxgiFactory->CreateSwapChain(m_Dev.p, &swapChainDesc, &m_SwapChain);
+    assert(SUCCEEDED(hr));
+
+    hr = m_SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&m_SwapChainTexture);
+    assert(SUCCEEDED(hr));
+    hr = m_Dev->CreateRenderTargetView(m_SwapChainTexture, NULL, &m_SwapChainRTV);
+    assert(SUCCEEDED(hr));
 }
 
 LRESULT CApp::WndProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch(msg)
     {
-    case WM_SYSCOMMAND:
-        if((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
-            return 0;
-        break; 
+    case WM_KEYDOWN:
+        if(wParam == VK_ESCAPE)
+            Exit();
+        return 0;
     }
 
     return DefWindowProc(wnd, msg, wParam, lParam);
@@ -89,6 +131,10 @@ void CApp::Exit()
 
 void CApp::Frame()
 {
+    vec4 clearColor = vec4(0.f, 0.f, 0.4f, 1.f);
+    m_Ctx->ClearRenderTargetView(m_SwapChainRTV, clearColor);
+
+    m_SwapChain->Present(1, 0);
 }
 
 LRESULT WINAPI WndProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -108,13 +154,20 @@ LRESULT WINAPI WndProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
         }
         return 0;
 
+    case WM_SYSCOMMAND:
+        // Disable ALT application menu.
+        if((wParam & 0xfff0) == SC_KEYMENU)
+            return 0;
+        break; 
+
     default:
         if(g_App)
         {
             return g_App->WndProc(wnd, msg, wParam, lParam);
         }
-        return DefWindowProc(wnd, msg, wParam, lParam);
     }
+
+    return DefWindowProc(wnd, msg, wParam, lParam);
 }
 
 static void Main2()
@@ -139,7 +192,6 @@ static void Main2()
     const DWORD wndExStyle = 0;
 
     ivec2 pos = ivec2(CW_USEDEFAULT, CW_USEDEFAULT);
-    ivec2 size = ivec2(CW_USEDEFAULT, CW_USEDEFAULT);
 
     HWND wnd = CreateWindowEx(
         wndExStyle,
@@ -147,7 +199,7 @@ static void Main2()
         WINDOW_TITLE,
         wndStyle,
         pos.x, pos.y,
-        size.x, size.y,
+        (int)DISPLAY_SIZE.x, (int)DISPLAY_SIZE.y,
         NULL,
         NULL,
         g_Instance,
